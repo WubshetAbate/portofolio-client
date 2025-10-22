26245fd1141a9970699a777d9ac6e34c8cfbe6ad
wube0965 <wubshetabate865@gmail.com>
2025-10-22 20:16:49 +0300

    last

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,20 +7,17 @@ export default function About() {
       <h2 className={styles.title}>About me</h2>
       <p className={styles.description}>
         Before transitioning into software development, I worked as an
-        Anesthetist, a role that honed my analytical thinking, precision, and
-        teamwork—skills that now guide my approach to programming. My medical
-        background taught me to remain calm under pressure, think critically,
-        and pay close attention to details—all invaluable traits when solving
-        complex coding problems and building reliable software. I began my
-        software development journey at Evangadi Tech, where I was awarded a
-        full scholarship to study modern web development. During this program, I
-        gained hands-on experience with essential tools and technologies,
-        including HTML, CSS, JavaScript, React, Node.js, Express, APIs, and
-        MySQL. I contributed to multiple projects, such as rebuilding the Apple
-        website, creating Netflix and Amazon clones, developing a Q&A platform,
-        and building smaller applications like a calculator. These experiences
-        not only strengthened my technical skills but also fueled my passion for
-        problem-solving and creating efficient, user-focused applications.
+        anesthetist, a role that sharpened my analytical thinking, precision,
+        and teamwork—skills I now apply to programming. My medical background
+        taught me to stay calm under pressure, think critically, and focus on
+        details, all essential for solving complex coding problems. I began my
+        development journey at Evangadi Tech with a full scholarship in modern
+        web development, gaining hands-on experience with HTML, CSS, JavaScript,
+        React, Node.js, Express, APIs, and MySQL. I contributed to projects like
+        rebuilding the Apple website, creating Netflix and Amazon clones,
+        developing a Q&A platform, and building smaller apps like a calculator.
+        These experiences strengthened my technical skills and fueled my passion
+        for problem-solving and building user-focused applications.
       </p>
 
       <div className={styles.stats}>
